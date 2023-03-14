6463402036c101d37fd8b979d95da002a982f545
aa <2419805602@qq.com>
2023-03-14 23:41:23 +0800

    home win8 ideaIU

---
+++ src/main/java/com/itheima/web/ServletDemo1.java
@@ -0,0 +1,29 @@
+package com.itheima.web;
+
+import com.itheima.pojo.Brand;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/Demo1")
+public class ServletDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 准备数据
+        List<Brand> brands = new ArrayList<Brand>();
+        brands.add(new Brand(1,"三只松鼠","三只松鼠",100,"三只松鼠，好吃不上火",1));
+        brands.add(new Brand(2,"优衣库","优衣库",200,"优衣库，服适人生",0));
+        brands.add(new Brand(3,"小米","小米科技有限公司",1000,"为发烧而生",1));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
